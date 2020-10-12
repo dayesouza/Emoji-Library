5292734718ee76d62618e22c677fd9353cd9c581
dayesouza <day.carol18@gmail.com>
2020-10-12 15:56:01 -0300

    update readme

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -4,8 +4,9 @@ import PropTypes from "prop-types";
 export default function EmojiCard({ obj }) {
   console.log(obj);
   return (
-    <div className="card card--hover">
+    <div className="card card--hover card__emoji">
       <h1>{obj.character}</h1>
+      <span className="card__emoji__name">{obj.unicodeName}</span>
     </div>
   );
 }
